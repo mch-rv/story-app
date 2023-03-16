2f85d67f859b95ae74f5fb2fea6ba7e4bfa14fd3
Mochammad Ravly <mohamadravly@gmail.com>
2023-03-16 19:34:30 +0700

    localize japan

--- src/generated/locales/en.js
+++ src/generated/locales/en.js
@@ -1,22 +1,16 @@
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize
 
-    // Do not modify this file by hand!
-    // Re-generate this file by running lit-localize
+/* eslint-disable no-irregular-whitespace */
 
-    
-    
-
-    /* eslint-disable no-irregular-whitespace */
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-
-    export const templates = {
-      's229fb4df9ecf3ed1': `Logout`,
-'s45dab30b4d2d3c14': `Login`,
-'s749c72374d54863f': `Close`,
-'s772ce5f935d58e37': `Description`,
-'s8d9d6501de2bbd98': `Add Data`,
-'sc72cca9281ef6073': `Date`,
-'se296c068d76c3b02': `Built with ❤ by Mochammad Ravly`,
-'sef49b2c68fd1e332': `Name`,
-'sf9a862bfd829397e': `Dashboard`,
-    };
-  
+export const templates = {
+  s229fb4df9ecf3ed1: `Logout`,
+  s45dab30b4d2d3c14: `Login`,
+  s749c72374d54863f: `Close`,
+  s772ce5f935d58e37: `Description`,
+  s8d9d6501de2bbd98: `Add Data`,
+  sc72cca9281ef6073: `Date`,
+  se296c068d76c3b02: `Built with ❤ by Mochammad Ravly`,
+  sef49b2c68fd1e332: `Name`,
+  sf9a862bfd829397e: `Dashboard`,
+};
--- src/generated/locales/jp.js
+++ src/generated/locales/jp.js
@@ -1,22 +1,16 @@
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize
 
-    // Do not modify this file by hand!
-    // Re-generate this file by running lit-localize
+/* eslint-disable no-irregular-whitespace */
 
-    
-    
-
-    /* eslint-disable no-irregular-whitespace */
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-
-    export const templates = {
-      's229fb4df9ecf3ed1': `ログイン`,
-'s45dab30b4d2d3c14': `ログイン`,
-'s749c72374d54863f': `閉まっている`,
-'s772ce5f935d58e37': `説明`,
-'s8d9d6501de2bbd98': `データを追加`,
-'sc72cca9281ef6073': `日にち`,
-'se296c068d76c3b02': `❤で作られました Mochammad Ravly`,
-'sef49b2c68fd1e332': `名前`,
-'sf9a862bfd829397e': `ダッシュボード`,
-    };
-  
+export const templates = {
+  s229fb4df9ecf3ed1: `ログイン`,
+  s45dab30b4d2d3c14: `ログイン`,
+  s749c72374d54863f: `閉まっている`,
+  s772ce5f935d58e37: `説明`,
+  s8d9d6501de2bbd98: `データを追加`,
+  sc72cca9281ef6073: `日にち`,
+  se296c068d76c3b02: `❤で作られました Mochammad Ravly`,
+  sef49b2c68fd1e332: `名前`,
+  sf9a862bfd829397e: `ダッシュボード`,
+};
